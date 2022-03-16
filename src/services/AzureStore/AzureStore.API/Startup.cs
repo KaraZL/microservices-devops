bfc439a6ecedaf21a3aadf2abe09ad1ccdefe21a
KaraZL <karazl.msnet@gmail.com>
2022-03-16 14:27:21 +0100

    application insights on AzureStore.API

--- src/services/AzureStore/AzureStore.API/Startup.cs
+++ src/services/AzureStore/AzureStore.API/Startup.cs
@@ -28,11 +28,18 @@ namespace AzureStore.API
         {
             services.AddScoped<IQueueService, QueueService>();
 
+            services.AddApplicationInsightsTelemetry(options =>
+            {
+                options.ConnectionString = Configuration["ApplicationInsights:InstrumentationKey"];
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AzureStore.API", Version = "v1" });
             });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
