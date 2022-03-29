53498dd2e863e2407e27e8c4931e4add48a2d307
KaraZL <karazl.msnet@gmail.com>
2022-03-29 13:43:17 +0200

    add .NET 6 series.API with Redis DB and Mapper

---
+++ src/services/Series.API/Data/ISeriesRepo.cs
@@ -0,0 +1,15 @@
+﻿using Series.API.Dto;
+using Series.API.Models;
+
+namespace Series.API.Data
+{
+    public interface ISeriesRepo
+    {
+        public IEnumerable<SeriesModel?>? GetAllSeries();
+        public SeriesModel? GetSeriesById(string id);
+        public void CreateSeries(CreateSeriesDto series);
+        public void UpdateSeries(SeriesModel series);
+        public void DeleteSeries(string id);
+
+    }
+}
---
+++ src/services/Series.API/Dto/CreateSeriesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Series.API.Dto
+{
+    public class CreateSeriesDto
+    {
+        public string Id { get; set; } = $"series:{Guid.NewGuid()}";
+        public string Name { get; set; } = string.Empty;
+        public DateTime DatePublished { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
